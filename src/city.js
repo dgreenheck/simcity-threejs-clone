56a5ec51edc8a7e073d1fbb04ed676040c57f6a2
Daniel Greenheck <dgreenheck@gmail.com>
2023-04-11 22:44:39 -0500

    Refactored create tile method

--- src/city.js
+++ src/city.js
@@ -7,24 +7,7 @@ export function createCity(size) {
     for (let x = 0; x < size; x++) {
       const column = [];
       for (let y = 0; y < size; y++) {
-        const tile = { 
-          x, 
-          y,
-          terrain: 'grass',
-          building: undefined,
-          update() {
-            const x = Math.random();
-            if (x < 0.01) {
-              if (this.building === undefined) {
-                this.building = 'building-1';
-              } else if (this.building === 'building-1') {
-                this.building = 'building-2';
-              } else if (this.building === 'building-2') {
-                this.building = 'building-3';
-              }
-            }
-          }
-        };
+        const tile = createTile(x, y);
         column.push(tile);
       }
       data.push(column);
@@ -44,5 +27,26 @@ export function createCity(size) {
     size,
     data,
     update
+  }
+}
+
+function createTile(x, y) {
+  return { 
+    x, 
+    y,
+    terrain: 'grass',
+    building: undefined,
+    update() {
+      const x = Math.random();
+      if (x < 0.01) {
+        if (this.building === undefined) {
+          this.building = 'building-1';
+        } else if (this.building === 'building-1') {
+          this.building = 'building-2';
+        } else if (this.building === 'building-2') {
+          this.building = 'building-3';
+        }
+      }
+    }
   }
 }
