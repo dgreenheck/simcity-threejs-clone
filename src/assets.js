2082f9912f866fc1195cab5d91d328117125b7af
Daniel Greenheck <dgreenheck@gmail.com>
2023-04-21 14:53:04 -0500

    Minor tweaks

--- src/assets.js
+++ src/assets.js
@@ -4,21 +4,21 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 const assets = { 
   'grass': (x, y) => {
-    const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
+    const material = new THREE.MeshLambertMaterial({ color: 0x339933 });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.userData = { id: 'grass' };
     mesh.position.set(x, -0.5, y);
     return mesh;
   },
   'building-1': (x, y) => {
-    const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+    const material = new THREE.MeshLambertMaterial({ color: 0xbb5555 });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.userData = { id: 'building-1' };
     mesh.position.set(x, 0.5, y);
     return mesh;
   },
   'building-2': (x, y) => {
-    const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+    const material = new THREE.MeshLambertMaterial({ color: 0xbbbb55 });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.userData = { id: 'building-2' };
     mesh.scale.set(1, 2, 1);
@@ -26,7 +26,7 @@ const assets = {
     return mesh;
   },
   'building-3': (x, y) => {
-    const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+    const material = new THREE.MeshLambertMaterial({ color: 0x5555bb });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.userData = { id: 'building-3' };
     mesh.scale.set(1, 3, 1);
